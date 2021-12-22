8c515842c307b5d14526719312e1b6305ab08e55
ProgFadi <fadiramzi.99@gmail.com>
2021-12-22 20:13:46 +0300

    4 migration files added by me

---
+++ database/migrations/2021_12_22_165655_create_cities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCitiesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cities', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->integer('gover_id')->unsigned();
+            $table->timestamps();
+
+            // FK
+            $table->foreign('gover_id')->references('id')->on('governorates');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cities');
+    }
+}
---
+++ database/migrations/2021_12_22_170522_create_sectors_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSectorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sectors', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name');
+            $table->unsignedInteger('center_id');//fk
+            $table->timestamps();
+
+            $table->foreign('center_id')->references('id')->on('centers');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sectors');
+    }
+}
